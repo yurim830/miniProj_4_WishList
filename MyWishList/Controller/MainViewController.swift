e1b210da25224e61d38116596dfec39b12b38993
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-16 16:49:06 +0900

    MVC 패턴, MainVC 연결

---
+++ MyWishList/Controller/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  MyWishList
+//
+//  Created by 유림 on 4/16/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
