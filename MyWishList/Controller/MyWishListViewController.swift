bf5685135cfc419526f656766bcdc439f0b85ac8
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-18 15:42:10 +0900

    #4 - 스토리보드와 코드 연결

---
+++ MyWishList/Controller/MyWishListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyWishListViewController.swift
+//  MyWishList
+//
+//  Created by 유림 on 4/18/24.
+//
+
+import UIKit
+
+class MyWishListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
