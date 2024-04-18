92092bcb1314a4ae8684bf16b28d4df5c67b8ef9
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-18 16:48:01 +0900

    \#4 - 버튼 클릭 시 wishList 페이지 present

--- MyWishList/Controller/MyWishListViewController.swift
+++ MyWishList/Controller/MyWishListViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class MyWishListViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView! // IBOutlet으로 연결
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.dataSource = self // dataSource 설정
     }
+    
+}
+
+extension MyWishListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     
 }
